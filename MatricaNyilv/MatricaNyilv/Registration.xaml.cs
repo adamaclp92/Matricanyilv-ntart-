4cad458d78f82063a180a0bb1f38499b1129a929
adamaclp92 <adamaclp92@gmail.com>
2021-03-11 22:07:35 +0100

    Add user procedure UserLevelList procedure UserLevelList combo box

--- MatricaNyilv/MatricaNyilv/Registration.xaml.cs
+++ MatricaNyilv/MatricaNyilv/Registration.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Configuration;
+using System.Data;
 using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
@@ -55,30 +56,49 @@ namespace MatricaNyilv
             }));
         }
 
+        void Load(string procedure, string display, string selected, ComboBox combo)
+        {
+            con.Open();
+            SqlCommand com = new SqlCommand(procedure, con);
+            com.CommandType = CommandType.StoredProcedure;
+            SqlDataReader reader = com.ExecuteReader();
+            DataTable dt = new DataTable();
+            dt.Load(reader);
+            combo.ItemsSource = dt.DefaultView;
+            combo.DisplayMemberPath = display;
+            combo.SelectedValuePath = selected;
+            con.Close();
+
+        }
 
         private void confirmbtn_Click(object sender, RoutedEventArgs e)
         {
-            /*if (usernametextbox.Text != "" && passwordtextbox.Password != "" && passwordconfirmtextbox.Password != "")  //validating the fields whether the fields or empty or not  
+            if (usernametextbox.Text != "" && passwordtextbox.Password != "" && passwordconfirmtextbox.Password != "")  //validating the fields whether the fields or empty or not  
             {
                 if (passwordtextbox.Password.ToString().Trim().ToLower() == passwordconfirmtextbox.Password.ToString().Trim().ToLower()) //validating Password textbox and confirm password textbox is match or unmatch    
                 {
                     string UserName = usernametextbox.Text;
                     string Password = Cryptohraphy.Encrypt(passwordtextbox.Password.ToString());   // Passing the Password to Encrypt method and the method will return encrypted string and stored in Password variable.  
                     con.Open();
-                    SqlCommand insert = new SqlCommand("insert into tblUserRegistration(UserName,Password)values('" + UserName + "','" + Password + "')", con);
-                    insert.ExecuteNonQuery();
+                    SqlCommand com = new SqlCommand("UserAdd", con);
+                    com.CommandType = CommandType.StoredProcedure;
+                    com.Parameters.AddWithValue("@username", UserName);
+                    com.Parameters.AddWithValue("@password", Password);
+                    com.Parameters.AddWithValue("@userlevel", userlevelcombo.SelectedValue);
+                    com.ExecuteNonQuery();
+                    
                     con.Close();
-                    MessageBox.Show("Record inserted successfully", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    MessageBox.Show("Record inserted successfully", "Success", MessageBoxButton.OK, MessageBoxImage.Information);
                 }
                 else
                 {
-                    MessageBox.Show("Password and Confirm Password doesn't match!.. Please Check..", "Error", MessageBoxButtons.OK, MessageBoxIcon.Information);  //showing the error message if password and confirm password doesn't match  
+                    MessageBox.Show("Password and Confirm Password doesn't match!.. Please Check..", "Error", MessageBoxButton.OK, MessageBoxImage.Information);  //showing the error message if password and confirm password doesn't match  
                 }
             }
             else
             {
-                MessageBox.Show("Please fill all the fields!..", "Error", MessageBoxButtons.OK, MessageBoxIcon.Information);  //showing the error message if any fields is empty  
-            }*/
+                MessageBox.Show("Please fill all the fields!..", "Error", MessageBoxButton.OK, MessageBoxImage.Information);  //showing the error message if any fields is empty  
+            }
         }
 
         private void Grid_MouseDown(object sender, MouseButtonEventArgs e)
@@ -89,6 +109,11 @@ namespace MatricaNyilv
         private void exitbutton_Click(object sender, RoutedEventArgs e)
         {
             this.Close();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            Load("UserLevelList", "Szerepkör", "Azonosító", userlevelcombo);
         }
     }
 }
